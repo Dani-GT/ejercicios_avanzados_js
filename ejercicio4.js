4d0d4c527548988eb8a886608d31ceb7f3336d26
Dani-GT <dani_spain87@hotmail.com>
2024-08-17 10:56:24 +0200

    4.2 done

--- ejercicio4.js
+++ ejercicio4.js
@@ -24,7 +24,18 @@ let posicion = findArrayIndex(mainCharacters, "Anakin");
 console.log(posicion);
 
 
-
-
-
 //4.2
+
+
+function removeItem(mainCharacters, text) {
+
+  const index = findArrayIndex(mainCharacters, text);
+  if (index !== -1) {
+      mainCharacters.splice(index, 1);
+  }
+  return mainCharacters;
+}
+
+
+console.log(removeItem(mainCharacters, "Leia")); 
+
