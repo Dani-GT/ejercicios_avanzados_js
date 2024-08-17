33909f1345fb0faa63586be8a9dba3a46466c922
Dani-GT <dani_spain87@hotmail.com>
2024-08-17 11:15:33 +0200

    done ejer 6

--- ejercicio6.js
+++ ejercicio6.js
@@ -4,3 +4,15 @@ const fantasticFour = [
     "La mujer invisible",
     "La cosa",
   ];
+
+  function swap(fantasticFour, index1, index2) {
+
+    const temp = fantasticFour[index1];
+    fantasticFour[index1] = fantasticFour[index2];
+    fantasticFour[index2] = temp;
+    
+    return fantasticFour;
+  }
+  
+  console.log(swap(fantasticFour, 1, 3)); 
+  
